4d98c709b67ea4354de82f2e0d77b84e8fd80441
Jamesstahl98 <92599615+Jamesstahl98@users.noreply.github.com>
2025-04-15 11:51:43 +0200

    Add admin page (dummy data, API calls commented out)

---
+++ Frontend/Models/Cart.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Frontend.Models
+{
+    public class Cart
+    {
+        public string Id { get; set; }
+
+        public User User { get; set; }
+
+        public List<CartProductDto> CartItems { get; set; }
+    }
+}
---
+++ Frontend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Frontend.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; } //placeholder property delete later
+
+        public string Role { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Address { get; set; }
+    }
+}
