bb5e759a3064ae86327c8b4edff5dce4b1c4c9f6
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-04-24 12:00:35 +0200

    Add ReceiptPopup

--- Frontend/wwwroot/js/stripeInterop.js
+++ Frontend/wwwroot/js/stripeInterop.js
@@ -1,23 +1,35 @@
 ﻿window.stripeInterop = (function () {
     let stripe;
-    let card;
+    let cardNumber, cardExpiry, cardCvc, postalCode;
 
     function initStripe(publishableKey) {
         stripe = Stripe(publishableKey);
         const elements = stripe.elements();
-        card = elements.create("card");
-        card.mount("#card-element");
 
-        card.on("change", function (event) {
-            const displayError = document.getElementById("card-errors");
-            displayError.textContent = event.error ? event.error.message : "";
+        cardNumber = elements.create("cardNumber");
+        cardNumber.mount("#card-number-element");
+
+        cardExpiry = elements.create("cardExpiry");
+        cardExpiry.mount("#card-expiry-element");
+
+        cardCvc = elements.create("cardCvc");
+        cardCvc.mount("#card-cvc-element");
+
+        postalCode = elements.create("postalCode");
+        postalCode.mount("#card-postal-element");
+
+        [cardNumber, cardExpiry, cardCvc, postalCode].forEach(element => {
+            element.on("change", function (event) {
+                const displayError = document.getElementById("card-errors");
+                displayError.textContent = event.error ? event.error.message : "";
+            });
         });
     }
 
     async function createPaymentMethod(name, email) {
         const result = await stripe.createPaymentMethod({
             type: "card",
-            card: card,
+            card: cardNumber,
             billing_details: {
                 name: name,
                 email: email
