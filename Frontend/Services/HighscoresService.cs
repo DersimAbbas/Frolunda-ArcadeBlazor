89f13e82d20013fb2bc89e8e7ac850bf0f18209e
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-04-28 16:11:50 +0200

    Add HighscoresService

---
+++ Frontend/Services/HighscoresService.cs
@@ -0,0 +1,37 @@
+﻿using Frontend.Models;
+using Frontend.Services.Interfaces;
+using System.Net.Http;
+
+namespace Frontend.Services
+{
+    public class HighscoresService(HttpClient httpClient) : IHighscoresService
+    {
+        public async Task<bool> AddNewHighscoresAsync(string name)
+        {
+            var result = await httpClient.PostAsJsonAsync("api/highscores", name);
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteHighscoresAsync(string highscoresId)
+        {
+            var result = await httpClient.DeleteAsync($"api/highscores/{highscoresId}");
+            return result.IsSuccessStatusCode;
+        }
+
+        public async Task<List<Highscores>?> GetAllHighscoresAsync()
+        {
+            return await httpClient.GetFromJsonAsync<List<Highscores>>("api/highscores");
+        }
+
+        public async Task<Highscores?> GetHighscoresAsync(string highscoresId)
+        {
+            return await httpClient.GetFromJsonAsync<Highscores>($"api/highscores/{highscoresId}");
+        }
+
+        public async Task<bool> UpdateUserHighscoreAsync(string id, Highscores highscores)
+        {
+            var response = await httpClient.PostAsJsonAsync($"api/highscores/{id}/update-user-score", highscores);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Frontend/Services/Interfaces/IHighscoresService.cs
@@ -0,0 +1,12 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Services.Interfaces;
+
+public interface IHighscoresService
+{
+    Task<List<Highscores>?> GetAllHighscoresAsync();
+    Task<Highscores?> GetHighscoresAsync(string highscoreId);
+    Task<bool> AddNewHighscoresAsync(string Name);
+    Task<bool> UpdateUserHighscoreAsync(string id, Highscores highscores);
+    Task<bool> DeleteHighscoresAsync(string highscoresId);
+}
