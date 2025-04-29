1b0568dc52641a810d706ea048c6445b5f4bc6b3
Jamesstahl98 <Jamesstahl556@gmail.com>
2025-04-29 14:55:58 +0200

    Add forum comment component

---
+++ Frontend/Models/ForumPost.cs
@@ -0,0 +1,13 @@
+﻿namespace Frontend.Models;
+
+public class ForumPost
+{
+    public string Id { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+    // public User Author { get; set; }
+    public User Author { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public ForumCategory Category { get; set; }
+    List<ForumComment> Comments { get; set; } = new();
+}
