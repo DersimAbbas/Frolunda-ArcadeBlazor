8fd5cd2230153e92f7d7d1df0e74c0e8b1be91d0
Ultravisitor <jonathanoalvin@gmail.com>
2025-04-30 10:16:36 +0200

    yes

---
+++ Frontend/Models/EventModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Frontend.Models
+{
+    public class EventModel
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public string Date { get; set; } = "";
+
+        public string Time { get; set; } = "";
+
+
+    }
+}
