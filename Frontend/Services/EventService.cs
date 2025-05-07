8566964e7785803f3f18e82cc2bcf17c16fda5c7
Ultravisitor <jonathanoalvin@gmail.com>
2025-05-07 15:19:18 +0200

    Started fixing EventService, fix later tonight

---
+++ Frontend/Services/EventService.cs
@@ -0,0 +1,40 @@
+﻿using Frontend.Services.Interfaces;
+using static Frontend.Components.User.Components.EventCarousel;
+
+namespace Frontend.Services
+{
+    public class EventService:IEventService
+    {
+        private readonly HttpClient _httpClient;
+        public EventService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<Models.Event> GetEventByIdAsync(string id)
+        {
+            return await _httpClient.GetFromJsonAsync<Models.Event>($"api/events/{id}");
+        }
+
+        public async Task<List<Event>?> GetAllEventsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Event>?>("api/events");
+        }
+
+        public async Task<bool> AddEvent(Event _event)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/events", _event);
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> UpdateEvent(string id, Event _event)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/events/{id}", _event);
+            return response.IsSuccessStatusCode;
+        }
+        public async Task<bool> DeleteEvent(string id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/events/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
---
+++ Frontend/Services/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using Frontend.Models;
+
+namespace Frontend.Services.Interfaces
+{
+    public interface IEventService
+    {
+        Task<Models.Event> GetEventByIdAsync(string id);
+        Task<List<Models.Event>?> GetAllEventsAsync();
+        Task<bool> AddEvent(Models.Event _event);
+        Task<bool> UpdateEvent(string id, Models.Event _event);
+        Task<bool> DeleteEvent(string id);
+    }
+}
